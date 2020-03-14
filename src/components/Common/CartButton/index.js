af055c50b9e09c31380dba0475e7d3852755afc7
Ayodele Olalekan <longyarnz@gmail.com>
2020-03-14 22:07:31 +0100

    Add badge to cart button

--- src/components/Common/CartButton/index.js
+++ src/components/Common/CartButton/index.js
@@ -7,6 +7,7 @@ function CartButton() {
   return (
     <button sx={styles.cartButton}>
       <img src={Cart} alt="cart" />
+      <span>88</span>
     </button>
   )
 }
@@ -17,6 +18,17 @@ const styles = {
   cartButton: {
     background: "transparent",
     border: "none",
-    cursor: "pointer"
+    cursor: "pointer",
+    position: "relative",
+    "& > span": {
+      position: "absolute",
+      p: '5px',
+      backgroundColor: "accent",
+      borderRadius: "50%",
+      color: "#fff",
+      fontWeight: "300",
+      fontSize: "10px",
+      right: "-8px"
+    }
   }
 }
