c7f5f5caa3267e26416dd64585af389d3d352622
Ayodele Olalekan <longyarnz@gmail.com>
2020-03-14 09:43:31 +0100

    Mockup Card component

--- src/components/HomePage/Card/index.js
+++ src/components/HomePage/Card/index.js
@@ -1,15 +1,29 @@
 /** @jsx jsx */
 import React from "react"
 import { jsx, Styled } from "theme-ui"
+import { Col } from "../../Grid"
+import Dummy from "../../../images/products/dumy.svg"
 
-function HomepageCard() {
+function HomepageCard({ title, src }) {
   return (
     <figure>
-
+      <img src={src} alt="product" sx={{ maxWidth: "1" }} />
+      <figcaption>
+        <caption>{title}</caption>
+        <p>
+          Something you truly need, but haven't know about yet!
+        </p>
+      </figcaption>
     </figure>
   )
 }
 
 export default HomepageCard
 
-const styles = {}
+const styles = {
+  imgWrapper: {
+    width: ["1", "1", "2/5", null, 450],
+    textAlign: ["center", "center", "left"],
+    order: [2, 2, 1],
+  }
+}
