fc881927e886ad4b6f42a56bf182ebac5045e1fd
Ayodele Olalekan <longyarnz@gmail.com>
2020-03-14 07:58:52 +0100

    Style text to match design

--- src/components/HomePage/Explore/index.js
+++ src/components/HomePage/Explore/index.js
@@ -1,18 +1,35 @@
 /** @jsx jsx */
 import React from "react"
-import { jsx } from "theme-ui"
+import { jsx, Styled } from "theme-ui"
+import { Row, Col } from "../../Grid"
 
 function HomepageExplore() {
   return (
-    <section>
-      <h2>
+    <section sx={styles.wrapper}>
+      <Styled.h2 sx={styles.heading}>
         Explore community choices
-      </h2>
-      <p>
-        Updated daily based on most popular choices among dev community
-      </p>
+      </Styled.h2>
+      <Styled.p sx={styles.subText}>
+        Updated daily based on most popular choices
+        <br />
+        among dev community
+      </Styled.p>
     </section>
   )
 }
 
 export default HomepageExplore
+
+const styles = {
+  wrapper: {
+    mt: 8
+  },
+  heading: {
+    fontFamily: "body",
+    fontWeight: "medium"
+  },
+  subText: {
+    fontWeight: "200",
+    color: "#fff"
+  }
+}
