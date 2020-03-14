3d53e47bae4dd3d751c74183a71777453fa9dc1e
Ayodele Olalekan <longyarnz@gmail.com>
2020-03-14 09:44:33 +0100

    Arrange mock cards in sldeshow

--- src/components/HomePage/Slideshow/index.js
+++ src/components/HomePage/Slideshow/index.js
@@ -1,15 +1,30 @@
 /** @jsx jsx */
 import React from "react"
 import { jsx, Styled } from "theme-ui"
+import { Row } from "../../Grid"
+import Card from "../Card"
+import Dummy from "../../../images/products/dumy.svg"
 
 function HomepageSlideshow() {
   return (
-    <section>
-
-    </section>
+    <Row>
+      <section sx={styles.wrapper}>
+        <Card title="Great Dummy" src={Dummy} />
+        <Card title="Basic Dummy" src={Dummy} />
+        <Card title="Dummy X Pro" src={Dummy} />
+        <Card title="Woooden Dummy" src={Dummy} />
+      </section>
+    </Row>
   )
 }
 
 export default HomepageSlideshow
 
-const styles = {}
+const styles = {
+  wrapper: {
+    width: '100%',
+    display: 'flex',
+    flexDirection: ['column', , ,'row'],
+    mt: 5
+  }
+}
