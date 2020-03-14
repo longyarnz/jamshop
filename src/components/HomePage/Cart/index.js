9b7da59b6ea2f7a44eec06e466e642c0a05405c4
Ayodele Olalekan <longyarnz@gmail.com>
2020-03-14 22:21:00 +0100

    Rename CartButton to Cart

--- src/components/HomePage/Cart/index.js
+++ src/components/HomePage/Cart/index.js
@@ -3,7 +3,7 @@ import React from "react"
 import { jsx } from "theme-ui"
 import Cart from "../../../images/elements/cart.svg"
 
-function CartButton() {
+function Cart() {
   return (
     <div>
       <button sx={styles.cartButton}>
@@ -14,7 +14,7 @@ function CartButton() {
   )
 }
 
-export default CartButton
+export default Cart
 
 const styles = {
   cartButton: {
