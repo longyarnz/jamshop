cfe46006427d07748539eec96046a4a4671d5062
Ayodele Olalekan <longyarnz@gmail.com>
2020-03-16 20:21:01 +0100

    Fix memory leak while navigating from the product page

--- src/components/HomePage/Cart/index.js
+++ src/components/HomePage/Cart/index.js
@@ -1,8 +1,8 @@
 /** @jsx jsx */
-import React, { useState, useContext } from "react"
+import React, { useState, useContext, useEffect } from "react"
 import { jsx } from "theme-ui"
 import ShouldRender from "@bit/lekanmedia.shared-ui.internal.should-render";
-import Box from './Box'
+import Box from "./Box"
 import src from "../../../images/elements/cart.svg"
 import { CartContext } from "../../../store"
 
@@ -10,25 +10,33 @@ function Cart(props) {
   const [open, setOpen] = useState(false);
   const [{ items }] = useContext(CartContext)
 
+  useEffect(() => {
+    return () => {
+      document.body.style.overflow = "auto";
+      document.body.onclick = null
+    }
+  }, [])
+
+  const revert = (e) => {
+    if (e.target.nodeName !== "HEADER") return
+    setOpen(false);
+    props.blurScreen(false);
+    document.body.style.overflow = "auto";
+    document.body.onclick = null
+  }
+
   const toggleBox = items.length > 0 ? e => {
     setOpen(!open);
-    props.blurScreen(!open);
-    document.body.style.overflow = !open ? "hidden" : "auto";
-
-    document.body.addEventListener("click", function revert(e) {
-      if (e.target.nodeName !== 'HEADER') return;
-      setOpen(false);
-      props.blurScreen(false);
-      document.body.style.overflow = "auto";
-      document.body.removeEventListener("click", revert);
-    })
+    props.blurScreen(!open)
+    document.body.style.overflow = !open ? "hidden" : "auto"
+    document.body.onclick = !open ? revert : null
   } : null
 
   return (
     <div sx={styles.wrapper}>
       <button sx={styles.cartButton} onClick={toggleBox}>
         <img src={src} alt="cart" />
-        <span>{ items.length }</span>
+        <span>{items.length}</span>
       </button>
       <ShouldRender if={open}>
         <Box items={items} />
@@ -51,7 +59,7 @@ const styles = {
     zIndex: 3,
     "& > span": {
       position: "absolute",
-      p: '5px',
+      p: "5px 7px",
       backgroundColor: "accent",
       borderRadius: "50%",
       color: "#fff",
